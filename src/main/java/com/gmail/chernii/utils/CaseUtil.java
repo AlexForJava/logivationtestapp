78bf90dc377f1d2de2060a14e1e30811d6938a20
AlexForJava <hqq@ukr.net>
2019-07-19 20:31:13 +0300

    initial commit

--- src/main/java/com/gmail/chernii/utils/CaseUtil.java
+++ src/main/java/com/gmail/chernii/utils/CaseUtil.java
@@ -3,17 +3,14 @@ package com.gmail.chernii.utils;
 import com.gmail.chernii.model.Base;
 import com.gmail.chernii.model.Case;
 import com.gmail.chernii.model.Product;
-import org.apache.log4j.Logger;
 
 
 public class CaseUtil {
-    private static final Logger LOG = Logger.getLogger(CaseUtil.class);
 
     private CaseUtil(){}
 
     public static double calculateOccupiedSpaceRatio(Case aCase, Product product, int numberOfProducts) {
         double ratio = occupiedSpaceRatio(aCase, product, numberOfProducts);
-        LOG.info("id" + aCase.getId() + "  = "+ ratio);
         aCase.setOccupiedSpaceRatio(ratio);
         return ratio;
     }
@@ -26,11 +23,11 @@ public class CaseUtil {
         return widthCount * heightCount * depthCount;
     }
 
-    private static double occupiedSpaceRatio(Case aCase, Product product, int numberOfProducts) {
-        return ((double) (numberOfProducts * calculateVolume(product))) / calculateVolume(aCase);
-    }
-
     public static int calculateVolume(Base base) {
         return (base.getSizeX() * base.getSizeY() * base.getSizeZ());
+    }
+
+    private static double occupiedSpaceRatio(Case aCase, Product product, int numberOfProducts) {
+        return ((double) (numberOfProducts * calculateVolume(product))) / calculateVolume(aCase);
     }
 }
